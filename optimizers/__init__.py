5e45acf2c4b9473064bc505fc093c7836dca098b
Yui-Koi <requize23@proton.me>
2025-10-20 15:46:58 +0000

    add flash muon to path

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,4 +1,9 @@
-from .flash_muon.flash_muon import Muon as FlashMuon
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent / 'flash-muon'))
+
+from flash_muon import Muon as FlashMuon
 from .muon import Muon, zeropower_via_newtonschulz5
 
-__all__ = ["FlashMuon", "Muon", "zeropower_via_newtonschulz5"]
+__all__ = ["FlashMuon", "Muon", "zeropower_via_newtonschulz4"]
