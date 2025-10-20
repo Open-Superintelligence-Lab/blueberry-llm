54ca9c3ca87ae1c178fe0769cdc3f36566638a0e
Yui-Koi <requize23@proton.me>
2025-10-20 06:22:28 +0000

    feat: add flash muon submodule alongside muon impl

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,3 +1,4 @@
+from .flash_muon.flash_muon import Muon as FlashMuon
 from .muon import Muon, zeropower_via_newtonschulz5
 
-__all__ = ['Muon', 'zeropower_via_newtonschulz5']
+__all__ = ["FlashMuon", "Muon", "zeropower_via_newtonschulz5"]
